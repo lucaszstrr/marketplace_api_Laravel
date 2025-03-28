c313626f3b6bb74d726641f8f532c83b6e3bb00b
Lucas <lucaaszstr@gmail.com>
2025-03-28 19:42:16 -0300

    Model Address

---
+++ src/app/Models/Address.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'userId',
+        'street',
+        'number',
+        'zip',
+        'city',
+        'state',
+        'country',
+    ];
+
+    protected $table = "address";
+    
+    public function user(){
+        return $this->has(User::class);
+    }
+}
