b24c0463496f081836faa045415a35b26b43e7be
Lucas <lucaaszstr@gmail.com>
2025-03-30 15:25:18 -0300

    New routes added

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,37 +2,48 @@
 
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->group(function () {
-    //Rota para adicionar endereco
-    Route::post('/address', [AddressController::class, 'store']);
-    //Rota para atualizar o role de usuarios
-    Route::post('/updatetitle', [UserController::class, 'update']);
+    //---------------------------ROTAS RELACIONADAS A ADDRESSES--------------------------------------------------------------
+        //Rota para adicionar endereco
+        Route::post('/address', [AddressController::class, 'store']);
+
+
+
+    //---------------------------ROTAS RELACIONADAS A USUARIOS--------------------------------------------------------------
+        //Rota para atualizar o role de usuarios
+        Route::post('/updatetitle', [UserController::class, 'update']);
+
+
+
+    //---------------------------ROTAS RELACIONADAS A CATEGORIAS------------------------------------------------------------
+        //Rota para criar categorias novas
+        Route::post('/category', [CategoryController::class, 'store']);
+        //Rota para atualizar categorias
+        Route::put('/category/{id}', [CategoryController::class, 'update']);
+        //Rota para deletar categoria especifica
+        Route::delete('/category/{id}', [CategoryController::class, 'delete']);
+
+
+
 });
 
-//Rota para criar user
-Route::post('/user', [UserController::class, 'store']);
-
-//Rota para fazer login
-Route::post('/login', [UserController::class, 'login']);
-
-//Rota para adicionar endereco
-//Route::post('/adress', [AddressController::class, 'store']);
+//---------------------------ROTAS RELACIONADAS A USUARIOS------------------------------------------------------------
+    //Rota para criar user
+    Route::post('/user', [UserController::class, 'store']);
+    //Rota para fazer login
+    Route::post('/login', [UserController::class, 'login']);
 
 
-//rota teste
-//Route::post('/teste', [AddressController::class,'index']);
+
+//---------------------------ROTAS RELACIONADAS A CATEGORIAS------------------------------------------------------------
+    //Rota para pegar todas as categorias
+    Route::get('/categories', [CategoryController::class, 'showAll']);
+    //Rota para pegar uma categoria especifica
+    Route::get('/category/{id}', [CategoryController::class, 'showOne']);
+
 
