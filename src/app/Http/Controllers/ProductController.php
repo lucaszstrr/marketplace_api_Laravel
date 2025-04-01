a7895d012a9d31a3d65ac08237243e8f933cbcda
Lucas <lucaaszstr@gmail.com>
2025-04-01 11:49:36 -0300

    New features added

---
+++ src/app/Http/Controllers/ProductController.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $userLogged = Auth::user();
+
+        $userRole = $userLogged->role;
+
+        if($userRole == 'user'){
+            return response()->json([
+                "error" => "You are not a moderator or admin to create new products",
+                "message" => "You are not a moderator or admin to create new products"
+            ], 400);
+        }
+
+        $product = $request->validate([
+            'categoryId' => 'required | integer',
+            'name' => 'required | string',
+            'stock' => 'required | integer',
+            'price' => 'required | numeric'
+        ]);
+
+        $foundId = Category::find($product['categoryId'], 'id');
+
+        if(!$foundId){
+            return response()->json([
+                'error' => "This id doesn't exists",
+                'message' => "This id doesn't exists"
+            ],400);
+        }
+
+        $productName = $product["name"];
+
+        $product = Product::create($product);
+        
+        return response()->json([
+            "message" => "Product '$productName' was created succesfully",
+            $product
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show()
+    {
+        $products = Product::all();
+
+        return response()->json([
+            $products
+        ]);
+    }
+
+    public function showId(string $id){
+        $product = Product::find($id);
+
+        if(!$product){
+            return response()->json([
+                "error"=> "This id doesn't exists",
+                "message"=> "This id doesn't exists"
+            ],404);
+        }
+
+        return response()->json([
+            $product
+        ], 200);
+
+    }
+
+    public function delete(Request $request, string $id)
+    {
+        $userLogged = Auth::user();
+
+        $userRole = $userLogged->role;
+
+        if($userRole == 'user'){
+            return response()->json([
+                'error'=> 'Only moderators can delete products',
+                'message'=> 'Only moderators can delete products'
+            ], 400);
+        }
+
+        $product = Product::find($id);
+
+        if(!$product){
+            return response()->json([
+                "error" => "This id doesn't exists",
+                "message" => "This id doesn't exists"
+            ],404);
+        }
+
+        $productName = $product["name"];
+
+        $product->delete();
+
+        return response()->json([
+            "message" => "Product '$productName' was deleted"
+        ],200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $userLogged = Auth::user();
+
+        $userRole = $userLogged->role;
+
+        if($userRole == "user"){
+            return response()->json([
+                "error"=> "Only moderators can update products",
+                "message"=> "Only moderators can update products"
+            ], 400);
+        }
+
+        $product = Product::find($id);
+
+        if(!$product){
+            response()->json([
+                "error" => "Product id wasn't found",
+                "message"=> "Product id wasn't found"
+            ], 404);
+        }
+
+        $updateProduct = $request->validate([
+            'name' => 'required | string',
+            'stock' => 'required | integer',
+            'price' => 'required | numeric'
+        ]);
+
+        $product->name = $updateProduct['name'];
+        $product->stock = $updateProduct['stock'];
+        $product->price = $updateProduct['price'];
+        $product->save();
+
+        $userName = $userLogged->name;
+
+        return response()->json([
+            'message'=> "Product updated succesfully by $userName",
+            $product
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
