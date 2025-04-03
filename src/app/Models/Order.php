841ee7b845cdb4c499e1d20bd967f01b8061a090
Lucas <lucaaszstr@gmail.com>
2025-04-03 11:35:48 -0300

    Order Model

---
+++ src/app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function address(){
+        return $this->belongsTo(Address::class);
+    }
+
+    public function coupon(){
+        return $this->has(Coupon::class);
+    }
+}
