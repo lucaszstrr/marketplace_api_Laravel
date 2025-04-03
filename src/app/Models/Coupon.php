dd7ce50aab389d8607b683999f9daa173327f1bb
Lucas <lucaaszstr@gmail.com>
2025-04-03 11:35:09 -0300

    Coupon Model

---
+++ src/app/Models/Coupon.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coupon extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "code",
+        "startDate",
+        "endDate",
+        "discountPercentage",
+    ];
+
+    public function order(){
+        return $this->applied_in(Order::class); 
+    }
+}
