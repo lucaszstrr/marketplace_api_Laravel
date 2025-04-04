c8ce1c9f7db9721607cc022c05d5b54d480721f9
Lucas <lucaaszstr@gmail.com>
2025-04-04 11:03:22 -0300

    CRUD was made to coupons

--- src/app/Http/Controllers/CouponController.php
+++ src/app/Http/Controllers/CouponController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Coupon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CouponController extends Controller
 {
@@ -27,15 +29,106 @@ class CouponController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $userLogged = Auth::user();
+
+        $userRole = $userLogged->role;
+
+        if($userRole == 'user' || $userRole == 'moderator'){
+            return response()->json([
+                "error" => "Only admins can create new coupons"
+            ], 401);
+        }
+
+        $coupon = $request->validate([
+            "code" => "required | string",
+            "startDate" => "required | date",
+            "endDate" => "required | date",
+            "discountPercentage" => "required | numeric"
+        ]);
+
+        $couponName = $coupon['code'];
+
+        $coupon = Coupon::create($coupon);
+
+        return response()->json([
+            "message" => "Coupon $couponName created succesfully"
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show()
     {
-        //
+        $userLogged = Auth::user();
+
+        $userRole = $userLogged->role;
+
+        if($userRole == 'user' || $userRole == 'moderator'){
+            return response()->json([
+                "error" => "Only admins can create new coupons"
+            ], 401);
+        }
+
+        $coupon = Coupon::all();
+
+        return response()->json([
+            $coupon
+        ], 200);
+    }
+
+    public function showSpecific(string $id)
+    {
+        $userLogged = Auth::user();
+
+        $userRole = $userLogged->role;
+
+        if($userRole == 'user' || $userRole == 'moderator'){
+            return response()->json([
+                "error" => "Only admins can create new coupons"
+            ], 401);
+        }
+
+        $coupon = Coupon::find($id);
+
+        if(!$coupon){
+            return response()->json([
+                "error" => "This coupon doesn't exists"
+            ], 404);
+        }
+
+        return response()->json([
+            $coupon
+        ], 200);
+    }
+
+    public function delete(string $id)
+    {
+        $userLogged = Auth::user();
+
+        $userRole = $userLogged->role;
+
+        if($userRole == 'user' || $userRole == 'moderator'){
+            return response()->json([
+                "error" => "Only admins can create new coupons"
+            ], 401);
+        }
+
+        $coupon = Coupon::find($id);
+
+        if(!$coupon){
+            return response()->json([
+                "error"=> "Only admins can delete coupons"
+            ], 401);
+        }
+
+        $couponName = $coupon->name;
+
+        $coupon->delete();
+
+        return response()->json([
+            "message" => "Coupon $couponName deleted succesfully"
+        ], 200);
     }
 
     /**
@@ -51,7 +144,31 @@ class CouponController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $userLogged = Auth::user();
+
+        $userRole = $userLogged->role;
+
+        if($userRole == 'user' || $userRole == 'moderator'){
+            return response()->json([
+                "error" => "Only admins can create new coupons"
+            ], 401);
+        }
+
+        $validateCoupon = $request->validate([
+            "code" => "required | string",
+            "startDate" => "required | date",
+            "endDate" => "required | date",
+            "discountPercentage" => "required | numeric"
+        ]);
+
+        $coupon = Coupon::find($id);
+
+        $coupon->update($validateCoupon);
+
+        return response()->json([
+            "message" => "Coupon updated succesfully",
+            $validateCoupon
+        ], 201);
     }
 
     /**
