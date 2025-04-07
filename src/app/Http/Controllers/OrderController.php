e014c6ef6fd5759671c4f121a223878c234dec1b
Lucas <lucaaszstr@gmail.com>
2025-04-07 11:50:58 -0300

    Feature added - create new order

--- src/app/Http/Controllers/OrderController.php
+++ src/app/Http/Controllers/OrderController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
+use App\Models\Coupon;
 use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
 {
@@ -28,7 +33,57 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $userLogged = Auth::user();
+
+        $userId = $userLogged->id;
+
+        $request->validate([
+            "addressId" => "required | integer",
+            "couponId" => "nullable | integer"
+        ]);
+        
+        $userAddress = Address::where("id", $request->addressId)->where("userId", $userId)->first();
+
+        if(!$userAddress){
+            return response()->json([
+                "error" => "Invalid address"
+            ]);
+        }
+
+        $user = User::findOrFail($userId);
+
+        //Isso puxa os itens do carrinho do usuario logado
+        $userCart = $user->cart->cartItems;
+
+        $totalAmount = 0;
+
+        //Isso percorre cada item do cart e faz a soma dos valores dos produtos
+        foreach($userCart as $item){
+            $totalAmount += $item['quantity'] * $item['unitPrice'];
+        }
+
+        $foundCoupon = Coupon::where('id', $request->couponId)->first();
+
+        if($foundCoupon){
+            $percentage = $foundCoupon['discountPercentage'];
+
+            $discount = ($totalAmount * $percentage) / 100;
+
+            $totalAmount -= $discount;  
+        }
+
+        $order = Order::create([
+            "userId" => $userId,
+            "addressId" => $userAddress['id'],
+            "couponId" => $foundCoupon['id'],
+            "totalAmount" => $totalAmount
+        ]);
+
+
+        return response()->json([
+            "message" => "Order created",
+            $order
+        ]);
     }
 
     /**
