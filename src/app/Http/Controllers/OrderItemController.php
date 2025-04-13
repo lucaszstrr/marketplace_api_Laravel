62ee7e8add7a3cb7e0390a77ff77937fae031266
Lucas <lucaaszstr@gmail.com>
2025-04-13 15:06:13 -0300

    Feature added - add items

--- src/app/Http/Controllers/OrderItemController.php
+++ src/app/Http/Controllers/OrderItemController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Cart;
 use App\Models\CartItem;
+use App\Models\Coupon;
+use App\Models\Order;
 use App\Models\OrderItem;
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -34,28 +37,100 @@ class OrderItemController extends Controller
     {
         $userLogged = Auth::user();
 
-        //Pega o carrinho do usuário logado
         $user = User::findOrFail($userLogged->id);
 
+        //Pega o carrinho do usuário logado
         $userCart = $user->cart->cartItems;
 
         $validateOrderItems = $request->validate([
-            "productId" => "required | integer",
-            "quantity" => "required | integer"
+            "productId" => "required | integer"
         ]);
 
+        //Percorre cada item do carrinho do usuario e armazena o id em um array para fazer a validação
+        foreach($userCart as $item){
+            $carts[] = $item->id;
+        }
+
+        //Valida se o produto passado no request é um item do usuario
+        if(!in_array($validateOrderItems["productId"], $carts)){
+            return response()->json([
+                "error" => "Cannot add this item"
+            ], 401);
+        }
+
+        //Puxa o produto com base no id passado na validacao
+        $selectedItem = CartItem::where("id", $validateOrderItems["productId"])->first();
+
+        //Puxa o produto da tabela products
+        $product = Product::where("id", $selectedItem->productId)->first();
+
+
+        $totalAmount = 0;
+
+        $itemQuantity = $selectedItem->quantity;
+
+        $itemPrice = $selectedItem->unitPrice;
+
+        $totalAmount += $itemQuantity * $itemPrice;
+
+        //Puxa o order do usuario
+        $order = $user->order;
+
+        $order->totalAmount += $totalAmount;
+
+        $order->save();
+
+        $orderItem = OrderItem::create([
+            "orderId" => $order->id,
+            "productId" => $product->id,
+            "quantity"=> $selectedItem->quantity,
+            "unitPrice" => $selectedItem->unitPrice,
+        ]);
 
         return response()->json([
-            $userCart
-        ]);
+            "message" => "Product added to order",
+            $product->name,
+            $product->price
+        ],200);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(OrderItem $orderItem)
+    public function show()
     {
-        //
+        $userLogged = Auth::user();
+
+        $userId = $userLogged->id;
+
+        $order = Order::where("userId", $userId)->first();
+
+        $orderItems = OrderItem::where("orderId", $order->id)->get();
+
+        return response()->json([
+            $orderItems
+        ]);
+    }
+
+    public function showSpecific(string $id)
+    {
+        $userLogged = Auth::user();
+
+        $userId = $userLogged->id;
+
+        $order = Order::where("userId", $userId)->first();
+
+        $orderItems = OrderItem::where("id", $id)->where("orderId", $order->id)->get();
+
+        if ($orderItems->isEmpty()) {
+            return response()->json([
+                "error" => "Invalid item"
+            ], 401);
+        }
+
+        return response()->json([
+            $orderItems
+        ]);
     }
 
     /**
@@ -77,29 +152,71 @@ class OrderItemController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(OrderItem $orderItem)
+    public function delete(string $id)
     {
-        //
+        $userLogged = Auth::user();
+
+        $userId = $userLogged->id;
+
+        $user = User::findOrFail($userId);
+
+        //Isso puxa a order do usuario logado
+        $order = Order::where("userId", $userId)->first();
+
+        //Isso puxa os itens do pedido do usuário logado
+        $orderItems = OrderItem::where("orderId", $order->id)->get();
+
+        $itemId = [];
+
+        //Percorre os itens do pedido e armazena os id's em um array
+        foreach($orderItems as $item){
+            $itemId[] += $item->id;
+        }
+
+        if(!in_array($id, $itemId)){
+            return response()->json([
+                "error" => "Invalid item"
+            ]);
+        }
+
+        $orderId = OrderItem::find($id);
+
+        $order->totalAmount -= $orderId->unitPrice;
+        $order->save();
+        
+        $orderId->delete();
+
+        return response()->json([
+            "message" => "Item deleted succesfully"
+        ], 200);
     }
 }
 
 
-//Isso pega todos os itens do carrinho do usuario logado
-// $userCartItems = CartItem::where("cartId", $userCart->id)->get();
 
-// //Vê se o usuário tem um Order Items criado na tabela
-// $userOrderItem = OrderItem::find($userCart->id);
 
-// if(!$userOrderItem){
-//     $createOrderItems = OrderItem::create([
-//         "productId" => $validateOrderItems['productId'],
-//         "quantity" => $validateOrderItems['quantity']
+
+// $foundCoupon = Coupon::where('id', $order->couponId)->first();
+
+// if(!$foundCoupon){
+//     $order->save();
+
+//     $orderItem = OrderItem::create([
+//         "orderId" => $order->id,
+//         "productId" => $product->id,
+//         "quantity"=> $selectedItem->quantity,
+//         "unitPrice" => $selectedItem->unitPrice,
 //     ]);
+
+//     return response()->json([
+//         "message" => "Product added to order",
+//         $product->name,
+//         $product->price
+//     ],200);
 // }
 
-// $createOrderItems = OrderItem::create([
-//     "productId" => $validateOrderItems['productId'],
-//     "quantity" => $validateOrderItems['quantity']
-// ]);
+// $discount = $foundCoupon->discountPercentage / 100;
 
-// $userCart = $user->cart->cartItems;
+// $discountPrice = $order->totalAmount * $discount;
+
+// $order->totalAmount -= $discountPrice;
