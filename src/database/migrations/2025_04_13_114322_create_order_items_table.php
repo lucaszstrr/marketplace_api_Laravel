d9de884c3784e137e10bde87506d0e0c17fe4044
Lucas <lucaaszstr@gmail.com>
2025-04-13 15:08:30 -0300

    Order Items Table

---
+++ src/database/migrations/2025_04_13_114322_create_order_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->integer('orderId');
+            $table->foreignId('productId')->constrained()->on('products');
+            $table->integer('quantity');
+            $table->decimal('unitPrice', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
