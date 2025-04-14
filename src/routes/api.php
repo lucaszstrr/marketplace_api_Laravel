d0a3dde271d8e399cda0bb5652a912e2de3ff70a
Lucas <lucaaszstr@gmail.com>
2025-04-14 11:30:35 -0300

    Routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,79 +1,93 @@
 <?php
 
 use App\Http\Controllers\AddressController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CartItemController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CouponController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('auth:sanctum')->group(function () {
-    //---------------------------ROTAS RELACIONADAS A ADDRESSES-------------------------------
-        //Rota para adicionar endereco
+    //ADDRESSES
         Route::post('/address', [AddressController::class, 'store']);
-        //Rota para atualizar endereco do usuario logado
         Route::put('/address/{id}', [AddressController::class, 'update']);
-        //Rota para pegar todos os address do usuario logado
         Route::get('/addresses', [AddressController::class, 'show']);
-        //Rota para address especifico do usuario logado
         Route::get('/addresses/{id}', [AddressController::class, 'showSpecific']);
 
 
 
-    //---------------------------ROTAS RELACIONADAS A USUARIOS-------------------------------
-        //Rota para atualizar o role de usuarios
-        Route::post('/updatetitle', [UserController::class, 'update']);
-        //Mostrar todos os user
+    //USUARIOS
+        Route::post('/update-role', [UserController::class, 'update']);
         Route::get('/user', [UserController::class, 'show']);
-        //Mostrar user especifico
         Route::get('/user/{id}', [UserController::class, 'showId']);
-        //Ver perfil logado
         Route::get('/users/me', [UserController::class, 'profile']);
-        //Atualizar user logado
         Route::put('/users/me', [UserController::class, 'updateUserLogged']);
-        //Delete o user logado
         Route::delete('/users/me', [UserController::class, 'userDelete']);
 
 
-    //---------------------------ROTAS RELACIONADAS A CATEGORIAS-----------------------------
-        //Rota para criar categorias novas
+    //CATEGORIAS
         Route::post('/category', [CategoryController::class, 'store']);
-        //Rota para atualizar categorias
         Route::put('/category/{id}', [CategoryController::class, 'update']);
-        //Rota para deletar categoria especifica
         Route::delete('/category/{id}', [CategoryController::class, 'delete']);
 
 
-    //---------------------------ROTAS RELACIONADAS A PRODUTOS-------------------------------       
-        //Rota para criar novos produtos
+    //PRODUTOS  
         Route::post('/products', [ProductController::class, 'store']);
-        //Rota para deletar produto especifico
         Route::delete('/product/{id}', [ProductController::class, 'delete']);
-        //Rota para atualizar produto
         Route::put('/product/update/{id}', [ProductController::class, 'update']);
+
+
+    //CARRINHO   
+        Route::post('/cart', [CartItemController::class, 'create']);
+        Route::get('/cart-items', [CartItemController::class, 'show']);
+        Route::get('/cart', [CartItemController::class, 'showCart']);
+        Route::put('/cart-items', [CartItemController::class, 'update']);
+        Route::post('/cart/items', [CartItemController::class, 'store']);
+        Route::delete('/cart-items/{id}', [CartItemController::class, 'delete']);
+
+
+    //ORDER   
+        Route::post('/orders', [OrderController::class, 'store']);
+        Route::get('/orders', [OrderController::class, 'show']);
+        Route::get('/orders', [OrderController::class, 'showAll']);
+        Route::put('/orders/{id}', [OrderController::class, 'updateStatus']);
+        Route::delete('/orders/{id}', [OrderController::class, 'delete']);
+
+
+    //ORDER ITEMS   
+        Route::post('/order-items', [OrderItemController::class, 'store']);
+        Route::get('/order-items', [OrderItemController::class, 'show']);
+        Route::get('/order-items/{id}', [OrderItemController::class, 'showSpecific']);
+        Route::delete('/order-items/{id}', [OrderItemController::class, 'delete']);
+
+
+    //CUPONS   
+        Route::post('/coupon', [CouponController::class, 'store']);
+        Route::put('/coupon/{id}', [CouponController::class, 'update']);
+        Route::get('/coupon', [CouponController::class, 'show']);
+        Route::get('/coupon/{id}', [CouponController::class, 'showSpecific']);
+        Route::delete('/coupon/{id}', [CouponController::class, 'delete']);
 
 });
 
-//---------------------------ROTAS RELACIONADAS A USUARIOS-----------------------------
-    //Rota para criar user
-    Route::post('/user', [UserController::class, 'store']);
-    //Rota para fazer login
-    Route::post('/login', [UserController::class, 'login']);
+//AUTH
+Route::post('/user', [AuthController::class, 'create']);
+Route::post('/login', [AuthController::class, 'login']);
     
 
+//CATEGORIAS
+Route::get('/categories', [CategoryController::class, 'showAll']);
+Route::get('/category/{id}', [CategoryController::class, 'showOne']);
 
 
-//---------------------------ROTAS RELACIONADAS A CATEGORIAS---------------------------
-    //Rota para pegar todas as categorias
-    Route::get('/categories', [CategoryController::class, 'showAll']);
-    //Rota para pegar uma categoria especifica
-    Route::get('/category/{id}', [CategoryController::class, 'showOne']);
-
-
-//---------------------------ROTAS RELACIONADAS A PRODUTOS-----------------------------
-    //Mostrar todos os produtos
-    Route::get('/products', [ProductController::class, 'show']);
-    //Mostrar produto especifico
-    Route::get('/products/{id}', [ProductController::class, 'showId']);
+//PRODUTOS
+Route::get('/products', [ProductController::class, 'show']);
+Route::get('/products/{id}', [ProductController::class, 'showId']);
 
