9aa4d90280c507847ad35b6ef6aa777fa7d2969b
Lucas <lucaaszstr@gmail.com>
2025-04-15 11:12:36 -0300

    Routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -51,6 +51,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('/cart-items', [CartItemController::class, 'update']);
         Route::post('/cart/items', [CartItemController::class, 'store']);
         Route::delete('/cart-items/{id}', [CartItemController::class, 'delete']);
+        Route::delete('/cart-items-clear', [CartItemController::class, 'clearCart']);
 
 
     //ORDER   
