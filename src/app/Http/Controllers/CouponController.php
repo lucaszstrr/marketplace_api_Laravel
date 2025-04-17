4aa5e49869d8e5a32ae00f461dd0b17400b4c16c
Lucas <lucaaszstr@gmail.com>
2025-04-17 09:13:18 -0300

    Coupon controller - final version

--- src/app/Http/Controllers/CouponController.php
+++ src/app/Http/Controllers/CouponController.php
@@ -8,25 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class CouponController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $userLogged = Auth::user();
@@ -41,10 +22,14 @@ class CouponController extends Controller
 
         $coupon = $request->validate([
             "code" => "required | string",
-            "startDate" => "required | date",
-            "endDate" => "required | date",
             "discountPercentage" => "required | numeric"
         ]);
+
+        if($coupon["discountPercentage"] <= 0 || $coupon["discountPercentage"] > 100){
+            return response()->json([
+                "error" => "Discount percentage must be a valid value between 0 and 100"
+            ], 401);
+        }
 
         $couponName = $coupon['code'];
 
@@ -55,9 +40,6 @@ class CouponController extends Controller
         ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show()
     {
         $userLogged = Auth::user();
@@ -131,17 +113,6 @@ class CouponController extends Controller
         ], 200);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         $userLogged = Auth::user();
@@ -169,13 +140,5 @@ class CouponController extends Controller
             "message" => "Coupon updated succesfully",
             $validateCoupon
         ], 201);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
