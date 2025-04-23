319ee0eca83211cbc5d0351ba0e1a4b90ce9cf60
Lucas <lucaaszstr@gmail.com>
2025-04-23 09:36:18 -0300

    Product controller

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -6,6 +6,8 @@ use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
@@ -17,8 +19,7 @@ class ProductController extends Controller
 
         if($userRole == 'user'){
             return response()->json([
-                "error" => "You are not a moderator or admin to create new products",
-                "message" => "You are not a moderator or admin to create new products"
+                "error" => "You are not a moderator or admin to create new products"
             ], 400);
         }
 
@@ -27,21 +28,32 @@ class ProductController extends Controller
             'name' => 'required | string',
             'stock' => 'required | integer',
             'price' => 'required | numeric',
-            'coupon' => 'required | boolean'
+            'coupon' => 'required | boolean',
+            'image' => 'required | image',
         ]);
 
         $foundId = Category::find($product['categoryId'], 'id');
 
         if(!$foundId){
             return response()->json([
-                'error' => "This id doesn't exists",
-                'message' => "This id doesn't exists"
+                'error' => "This id doesn't exists"
             ],400);
         }
 
         $productName = $product["name"];
 
-        $product = Product::create($product);
+        $imageName = Str::uuid() . '.' . $request->file('image')->getClientOriginalExtension();
+
+        $path = Storage::putFileAs('products', $request->file('image'), $imageName);
+
+        $product = Product::create([
+            "categoryId" => $product['categoryId'],
+            "name" => $product['name'],
+            "stock" => $product['stock'],
+            "price" => $product['price'],
+            "coupon" => $product['coupon'],
+            "image" => $path
+        ]);
         
         return response()->json([
             "message" => "Product '$productName' was created succesfully",
