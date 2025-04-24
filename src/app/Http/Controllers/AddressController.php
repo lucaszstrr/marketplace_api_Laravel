b86bb2bef6529b5e8d7c8864fa128128c84ba6f5
Lucas <lucaaszstr@gmail.com>
2025-04-24 11:28:57 -0300

    Address controller - feature added validate zipcode

--- src/app/Http/Controllers/AddressController.php
+++ src/app/Http/Controllers/AddressController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Address;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 
 class AddressController extends Controller
 {
@@ -30,6 +31,34 @@ class AddressController extends Controller
             'state' => 'required | string',
             'country' => 'required | string',
         ]);
+
+        //Validação de CEP no ViaCep
+        $validateCep = Http::get("https://viacep.com.br/ws/{$validateAddress['zip']}/json/");
+
+        if($validateCep->failed() || isset($validateCep->json()['erro'])){
+            return response()->json([
+                "error" => "O CEP é inválido"
+            ], 401);
+        }
+
+        $number = $validateCep['complemento'];
+
+        if(preg_match("/[0-9]+/",$number, $matches)){
+            $first_number = $matches[0];
+        }
+
+        if(preg_match("/\d+$/", $number, $matches2)){
+            $last_number = $matches2[0];
+        }
+
+        $integer_first_number = (int)$first_number;
+        $integer_last_number = (int)$last_number;
+
+        if($validateAddress['number'] < $integer_first_number || $validateAddress['number'] > $integer_last_number){
+            return response()->json([
+                "error" => "O número é inválido"
+            ],401);
+        }
 
         //Isso pega o nome do user logado
         $userName = $userLogged->name;
